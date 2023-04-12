95d040e5d9a100f75556e83a503bc51d2a7bb547
Ivan Paez Coronell <ipaezcoronell@outlook.com>
2023-04-12 09:50:35 -0500

    chore: update favicon

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -6,10 +6,10 @@ export const MainLayout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>First app</title>
+        <title>Gestion de inventario</title>
         <meta name="description" content="First app with next" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Navbar />
       <main className={styles.main}>{children}</main>
