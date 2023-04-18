27eafa9015c94b0ae5c156c48f0400b3ecd34c13
Ivan Paez Coronell <ipaezcoronell@outlook.com>
2023-04-18 09:31:16 -0500

    feat: update navbar routes

---
+++ src/components/measures/MeasuresList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MeasuresList = () => {
+  return <div></div>
+}
+
+export default MeasuresList
---
+++ src/pages/measures/index.jsx
@@ -0,0 +1,13 @@
+import { MainLayout } from '@/layout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <MainLayout
+      title="Gestionar unidades de medidas"
+      description="pagina para gestionar las unidades de medidas"
+    ></MainLayout>
+  )
+}
+
+export default index
